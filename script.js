21df06a5e12c3945170136fe12a84b4b21f982d5
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-18 17:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  const newRow = document.createElement("tr");
-	const leftCell = document.createElement("td");
-	const rightCell = document.createElement("td");
+const table = document.getElementById("sampleTable");
+  const newRow = table.insertRow(0);
+  
+  const leftCell = newRow.insertCell(0);
+  const rightCell = newRow.insertCell(1);
 
-	leftCell.textContent = "New Cell1";
-	rightCell.textContent = "New Cell2";
-	
-	newRow.appendChild(leftCell);
-	newRow.appendChild(rightCell);
-
-	const table = document.getElementById("sampleTable");
-
-	if (table.rows.length > 0) {
-		table.deleteRow(0);
-	}
-
-	table.insertBefore(newRow,table.firstChild);
+  leftCell.textContent = "New Cell1";
+  rightCell.textContent = "New Cell2";
 }
 
 insert_Row();
